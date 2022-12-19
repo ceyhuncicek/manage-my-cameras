2486166ba2f5cc409999c4cdd8b0ab77c518d803
ceyhuncicek <40377224+ceyhuncicek@users.noreply.github.com>
2022-12-19 16:04:10 +0100

    added top toolbar component

---
+++ src/components/TopToolbar.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-toolbar dense>
+    <v-app-bar-nav-icon @click="toggleSidebar" />
+    <v-spacer></v-spacer>
+    <v-btn icon href="https://github.com/ceyhuncicek" target="_blank">
+      <v-icon>mdi-github</v-icon>
+    </v-btn>
+    <UserProfile />
+  </v-toolbar>
+</template>
+
+<script>
+import { mapActions } from 'vuex';
+import UserProfile from '@/components/UserProfile.vue';
+
+export default {
+  name: 'TopToolbar',
+  components: { UserProfile },
+  methods: {
+    ...mapActions('sidebar', ['toggleSidebar']),
+  },
+  computed: {},
+  data() {
+    return {
+      drawer: false,
+      items: [{ title: 'Dashboard', icon: 'mdi-view-dashboard' }],
+    };
+  },
+};
+</script>
+
+<style scoped></style>
