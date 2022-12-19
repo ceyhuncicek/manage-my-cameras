e9f9da27e3896f1e476aca00fe2c185b7eb2c778
ceyhuncicek <40377224+ceyhuncicek@users.noreply.github.com>
2022-12-19 18:04:08 +0100

    created LiveCamera component

---
+++ src/components/LiveCamera.vue
@@ -0,0 +1,56 @@
+<template>
+  <v-container grid-list>
+    <v-row>
+      <v-col cols="12" md="6">
+        <v-img class="rounded-xl" max-height="300" src="@/assets/camera-view.jpg"></v-img>
+      </v-col>
+      <v-col cols="12" md="6">
+        <v-list>
+          <v-list-item>
+            <v-list-item-content>
+              <v-list-item-title class="title">
+                {{ name }}
+              </v-list-item-title>
+              <v-list-item-subtitle>
+                <v-icon>mdi-video</v-icon>
+                {{ cameraId }}
+              </v-list-item-subtitle>
+              <v-list-item-subtitle>
+                <v-icon>mdi-server-network</v-icon>
+                {{ ethMacAddress }}
+              </v-list-item-subtitle>
+              <v-list-item-subtitle>
+                <v-icon>mdi-map</v-icon>
+                {{ zoneId }}
+              </v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'LiveCamera',
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    cameraId: {
+      type: Number,
+      required: true,
+    },
+    ethMacAddress: {
+      type: String,
+      required: true,
+    },
+    zoneId: {
+      type: Number,
+      required: true,
+    },
+  },
+};
+</script>
--- src/views/cameras/_id.vue
+++ src/views/cameras/_id.vue
@@ -1,50 +1,31 @@
 <template>
-  <v-container>
-    <v-container class="pa-0">
-      <v-row>
-        <v-col cols="12">
-          <v-btn text primary @click="openOverview">
-            <v-icon left>mdi-arrow-left</v-icon>
-            Back to overview
-          </v-btn>
-        </v-col>
-        <v-col cols="12" md="6">
-          <v-img class="rounded-xl" max-height="300" src="@/assets/camera-view.jpg"></v-img>
-        </v-col>
-        <v-col cols="12" md="6">
-          <v-list>
-            <v-list-item>
-              <v-list-item-content>
-                <v-list-item-title class="title">
-                  {{ cameraData.name }}
-                </v-list-item-title>
-                <v-list-item-subtitle>
-                  <v-icon>mdi-video</v-icon>
-                  {{ cameraData.cameraId }}
-                </v-list-item-subtitle>
-                <v-list-item-subtitle>
-                  <v-icon>mdi-server-network</v-icon>
-                  {{ cameraData.ethMacAddress }}
-                </v-list-item-subtitle>
-                <v-list-item-subtitle>
-                  <v-icon>mdi-map</v-icon>
-                  {{ cameraData.zoneId }}
-                </v-list-item-subtitle>
-              </v-list-item-content>
-            </v-list-item>
-          </v-list>
-        </v-col>
-      </v-row>
-    </v-container>
+  <v-container class="pa-0">
+    <v-row>
+      <v-col cols="12">
+        <v-btn text primary @click="openOverview">
+          <v-icon left>mdi-arrow-left</v-icon>
+          Back to overview
+        </v-btn>
+      </v-col>
+    </v-row>
+    <LiveCamera
+      v-if="cameraData"
+      :name="cameraData.name"
+      :cameraId="cameraData.cameraId"
+      :ethMacAddress="cameraData.ethMacAddress"
+      :zoneId="cameraData.zoneId" />
   </v-container>
 </template>
 
 <script>
+import LiveCamera from '@/components/LiveCamera.vue';
 import camerasService from '@/services/cameras.service';
 
 export default {
   name: 'HomeView',
-  components: {},
+  components: {
+    LiveCamera,
+  },
   computed: {
     cameraId() {
       return this.$route.params.id;
@@ -59,7 +40,7 @@ export default {
     },
   },
   data: () => ({
-    cameraData: {},
+    cameraData: undefined,
     loadingStatus: {
       camera: false,
     },
