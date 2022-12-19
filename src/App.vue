5cc03b0f9090de9651f40c5249179ecaa492a3c0
ceyhuncicek <40377224+ceyhuncicek@users.noreply.github.com>
2022-12-19 16:06:21 +0100

    added navigation drawer and toolbar component to app

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,26 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
+    <TopToolbar v-if="loggedIn" class="px-6 flex-grow-0" />
+    <NavigationDrawer />
+    <v-main class="w-100">
+      <div class="pa-6">
+        <router-view />
       </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
-      <router-view/>
     </v-main>
   </v-app>
 </template>
 
 <script>
+import NavigationDrawer from '@/components/NavigationDrawer.vue';
+import TopToolbar from '@/components/TopToolbar.vue';
 
 export default {
   name: 'App',
-
-  data: () => ({
-    //
-  }),
+  computed: {
+    loggedIn() {
+      return this.$store.state.auth.status.loggedIn;
+    },
+  },
+  components: { NavigationDrawer, TopToolbar },
 };
 </script>
