482dd10c333949c3ed7883ae315142757890320e
ceyhuncicek <40377224+ceyhuncicek@users.noreply.github.com>
2022-12-19 16:06:57 +0100

    added home view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,74 @@
 <template>
-  <hello-world />
+  <vCard>
+    <vCardText>
+      <vTextField
+        v-model="camerasTable.search"
+        append-icon="mdi-magnify"
+        label="Search"
+        single-line
+        hide-details></vTextField>
+    </vCardText>
+    <vDataTable
+      :headers="camerasTable.headers"
+      :items="camerasTable.items"
+      :search="camerasTable.search"
+      :loading="loadingStatus.cameras">
+      <template v-slot:[`item.actions`]="{ item }">
+        <v-btn small rounded color="secondary" @click="openDetails(item.cameraId)">View</v-btn>
+      </template>
+    </vDataTable>
+  </vCard>
 </template>
 
 <script>
-import HelloWorld from '../components/HelloWorld.vue';
+import camerasService from '@/services/cameras.service';
 
 export default {
-  name: 'MyHome',
-
-  components: {
-    HelloWorld,
+  name: 'HomeView',
+  components: {},
+  methods: {
+    openDetails(cameraId) {
+      this.$router.push({ name: 'cameras-id', params: { id: cameraId } });
+    },
+    getCameras() {
+      this.loadingStatus.cameras = true;
+      camerasService
+        .get()
+        .then((cameras) => {
+          this.camerasTable.items = cameras;
+        })
+        .catch((error) => {
+          console.log(error);
+        })
+        .finally(() => {
+          this.loadingStatus.cameras = false;
+        });
+    },
+  },
+  data: () => ({
+    loadingStatus: {
+      cameras: false,
+    },
+    camerasTable: {
+      search: '',
+      headers: [
+        {
+          text: '#',
+          align: 'start',
+          value: 'cameraId',
+        },
+        { text: 'Name', value: 'name' },
+        { text: 'Device type', value: 'deviceTypeId' },
+        { text: 'Mac adress', value: 'ethMacAddress' },
+        { text: 'Zone id', value: 'zoneId' },
+        { text: 'Account id', value: 'accountId' },
+        { text: '', value: 'actions', align: 'end' },
+      ],
+      items: [],
+    },
+  }),
+  mounted() {
+    this.getCameras();
   },
 };
 </script>
