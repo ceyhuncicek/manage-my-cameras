f8ad8d36783754529c53308289457264baf8abfd
ceyhuncicek <40377224+ceyhuncicek@users.noreply.github.com>
2022-12-19 16:06:03 +0100

    added navigation drawer component

---
+++ src/components/NavigationDrawer.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <v-navigation-drawer
+      app
+      @input="setSidebarState"
+      :value="isSidebarActive"
+      :permanent="$vuetify.breakpoint.mdAndUp">
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="text-h6">Manage my cameras</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list dense nav>
+        <v-list-item v-for="item in items" :key="item.title" link>
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title></v-list-item-title>
+            <v-btn text primary :to="item.to">{{ item.title }}</v-btn>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+  </div>
+</template>
+
+<script>
+import { mapState, mapActions } from 'vuex';
+
+export default {
+  name: 'NavigationDrawer',
+  components: {},
+  methods: {
+    ...mapActions('sidebar', ['setSidebarState']),
+  },
+  computed: {
+    ...mapState('sidebar', ['isSidebarActive']),
+  },
+  data() {
+    return {
+      items: [{ title: 'Dashboard', to: '/home', icon: 'mdi-view-dashboard' }],
+    };
+  },
+};
+</script>
+
+<style scoped></style>
